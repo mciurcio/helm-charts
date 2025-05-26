1fef36e7efcf479f46a5f4e44c7d25b130859378
Miguel Di Ciurcio Filho <miguel@cps1.tech>
2025-05-26 16:49:39 -0300

    fix(contrib): fix nodejs typo

--- charts/cps1-contrib/scripts/packages/nodejs.sh
+++ charts/cps1-contrib/scripts/packages/nodejs.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 HOME=/home/user
 
 # shellcheck disable=SC2223
-: ${CPS1_NODE_VERSION:="v22"}
+: ${CPS1_NODEJS_VERSION:="v22"}
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
@@ -14,8 +14,8 @@ export NVM_DIR="$HOME/.nvm"
 # shellcheck disable=SC1091
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-nvm install "${CPS1_NODE_VERSION}"
-nvm alias default "${CPS1_NODE_VERSION}"
+nvm install "${CPS1_NODEJS_VERSION}"
+nvm alias default "${CPS1_NODEJS_VERSION}"
 
 chown -R user:user /home/user/.nvm
 
